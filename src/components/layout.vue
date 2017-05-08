444858d2820bd1678fb78cb7408b6f6ff3cbd2cf
力力娃 <liliwa@liliwadeMacBook-Pro.local>
2017-05-08 11:24:36 +0800

    pc项目练习start；

---
+++ src/components/layout.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="container">
+		<!-- 头部 -->
+		<div class="layout">
+			<nav class="navbar navbar-default" role="navigation">
+			  <!-- Brand and toggle get grouped for better mobile display -->
+			  <div class="navbar-header">
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			      <span class="sr-only">Toggle navigation</span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			    <a class="navbar-brand" href="#">Brand</a>
+			  </div>
+
+			  <!-- Collect the nav links, forms, and other content for toggling -->
+			  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			    <ul class="nav navbar-nav navbar-right">
+			      <li><a href="#">登录</a></li>
+			      <li><a href="#">注册</a></li>
+			      <li><a href="#">关于</a></li>
+			    </ul>
+			  </div><!-- /.navbar-collapse -->
+			</nav>
+		</div>
+		<!-- content-->
+		<div class="content">
+			<keep-alive>
+     	 	  <router-view></router-view>
+   		    </keep-alive>
+		</div>
+		<!--底部-->
+		<div class="foot">
+			 <p class="text-center">© 2016 fishenal MIT</p>
+		</div>
+	</div>
+</template>
